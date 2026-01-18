4d06db9f550ed4b0826026f165a7b7a9fabb34cb
Darina <darina240705@gmail.com>
2026-01-18 21:09:46 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
